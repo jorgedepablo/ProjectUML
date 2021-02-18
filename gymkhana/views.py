f5777edbb2670533605fc2034cc2337561b93733
Jorge De Pablo <36046965+jorgedepablo@users.noreply.github.com>
2021-02-18 22:33:14 +0100

    adding some logic in the first challenge

--- gymkhana/views.py
+++ gymkhana/views.py
@@ -6,12 +6,19 @@ def start(request):
 
     return HttpResponse('Init the gymkhana app...')
 
+
 def quest(request): 
 
-    return render(request, "first_question.html")
+    return render(request, 'first_question.html')
+
 
 def response(request): 
 
-    msg = "First challenge complete !  %r " %request.GET["surname"]
+    key = 'De Pablo'
+
+    if  str(request.GET['surname']) != key: 
+        msg = 'The surname %s is not good enough' %request.GET['surname']
+    else: 
+        msg = 'The best surname ever, Mr. %s' %request.GET['surname']
 
     return HttpResponse(msg)
