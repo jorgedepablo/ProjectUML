9b4367fe1c4dece6a67a7ead84dcf620edb6f802
Jorge De Pablo <36046965+jorgedepablo@users.noreply.github.com>
2021-03-12 00:22:11 +0100

    primera versiond e game 1

--- gymkhana/views.py
+++ gymkhana/views.py
@@ -8,30 +8,30 @@ def start(request):
 
 
 def quest_1(request): 
-
+    # diagrama de actividades
     return render(request, 'first_question.html')
 
 def quest_2(request): 
-
+    # Diagrama de casos de uso
     return render(request, 'first_question.html')
 
 def quest_3(request): 
-
+    #  Object Diagrams
     return render(request, 'first_question.html')
 
 
 def response_1(request): 
 
-    key = 'De Pablo'
+    key = 'Nal Hutta'
 
-    if str(request.GET['surname']) != key: 
+    if str(request.GET['keyword']) != key: 
         msg = 'The surname %s is not good enough' %request.GET['surname']
     else:
         msg = 'The best surname ever, Mr. %s' %request.GET['surname']
 
     return HttpResponse(msg)
 
-def response_3(request): 
+def response_2(request): 
 
     key = 'De Pablo'
 
