28573ae0e53950f0ded2f9f5ab3ca21db49bca36
Jorge De Pablo <36046965+jorgedepablo@users.noreply.github.com>
2021-03-17 20:05:10 +0100

    prueba con dos primeros juegos en static

--- gymkhana/views.py
+++ gymkhana/views.py
@@ -7,17 +7,17 @@ def start(request):
     return render(request, 'start.html')
 
 
-def quest_1(request): 
+def game_1(request): 
     # diagrama de actividades
-    return render(request, 'first_question.html')
+    return render(request, 'game_1.html')
 
-def quest_2(request): 
+def game_2(request): 
     # Diagrama de casos de uso
-    return render(request, 'first_question.html')
+    return render(request, 'game_2.html')
 
-def quest_3(request): 
+def game_3(request): 
     #  Object Diagrams
-    return render(request, 'first_question.html')
+    return render(request, 'game_3.html')
 
 
 def response_1(request): 
@@ -26,8 +26,11 @@ def response_1(request):
 
     if str(request.GET['keyword']) != key: 
         msg = 'The surname %s is not good enough' %request.GET['surname']
+        template = 'wrong.html'
     else:
         msg = 'The best surname ever, Mr. %s' %request.GET['surname']
+        # recondicr a success
+        template = 'success.html'
 
     return HttpResponse(msg)
 
