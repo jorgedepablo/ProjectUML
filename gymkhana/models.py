973e9a749e2c7bac118cf22238c3f860f06db268
Jorge De Pablo <36046965+jorgedepablo@users.noreply.github.com>
2021-05-22 11:54:17 +0200

    upgrade con BBDD 1: primera prubea de models

--- gymkhana/models.py
+++ gymkhana/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.db.models.fields import EmailField
+from django.db.models.signals import ModelSignal
 
 # Create your models here.
+
+class Challenge(models.Model): 
+    question = models.CharField()
+    awnser = models.CharField(max_length=100)
+    diagram = models.CharField(max_length=20) # el path del repo donde esté el diagrama 
+
+class Game(models.Model): 
+    title = models.CharField(max_length=50)
+    description = models.CharField()
+    challenge = models.ManyToManyField(Challenge)
+
+class Users(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    score = models.IntegerField() # posible desarrollo para que tengas puntos por resolver challenges
